647eab988aecf2722abdad301f6d1e2c4fc11f62
Josiah Barringer <59709224+17BarringerJosiah@users.noreply.github.com>
2024-10-10 11:22:55 -0400

    Add files via upload

---
+++ JB_Project7/JB_Project7/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.cmbQuery = New System.Windows.Forms.ComboBox()
+        Me.DataSet1 = New System.Data.DataSet()
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnSave
+        '
+        Me.btnSave.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnSave.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnSave.Location = New System.Drawing.Point(229, 287)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 0
+        Me.btnSave.Text = "&Save"
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'btnClose
+        '
+        Me.btnClose.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnClose.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnClose.Location = New System.Drawing.Point(471, 287)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 1
+        Me.btnClose.Text = "&Close"
+        Me.btnClose.UseVisualStyleBackColor = False
+        '
+        'cmbQuery
+        '
+        Me.cmbQuery.FormattingEnabled = True
+        Me.cmbQuery.Items.AddRange(New Object() {"New table for game preferences", "Add Cyberpunk 2077", "Delete Madden NFL 20", "Modify price for Resident Evil", "Sort games by price"})
+        Me.cmbQuery.Location = New System.Drawing.Point(340, 48)
+        Me.cmbQuery.Name = "cmbQuery"
+        Me.cmbQuery.Size = New System.Drawing.Size(121, 21)
+        Me.cmbQuery.TabIndex = 2
+        '
+        'DataSet1
+        '
+        Me.DataSet1.DataSetName = "NewDataSet"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.JB_Project7.My.Resources.Resources.vern
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.cmbQuery)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnSave)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents cmbQuery As ComboBox
+    Friend WithEvents DataSet1 As DataSet
+End Class
---
+++ JB_Project7/JB_Project7/Form1.vb
@@ -0,0 +1,86 @@
+﻿' Database Programming Assignment
+' Create a program that utilizes a database
+' Josiah Barringer
+' "I will not use code that was never covered in class or in our textbook. 
+' If you do you must be able to explain your code when asked by the professor. 
+' Using code outside of the resources provided, it can be flagged and 
+' reported as an academic integrity violation if there is any suspicion 
+' of copying/cheating."
+
+
+Imports System.Data.SqlClient
+
+Public Class Form1
+    ' Connection string to the "Galore" database in the Debug folder
+    Dim connString As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Galore.mdf;Integrated Security=True"
+    Dim conn As New SqlConnection(connString)
+    Dim dsGalore As New DataSet()
+
+    ' Method to load data from database and bind to textboxes
+    Private Sub LoadData()
+        Dim cmd As New SqlCommand("SELECT * FROM TableName", conn)
+        Dim adapter As New SqlDataAdapter(cmd)
+        adapter.Fill(dsGalore, "TableName")
+
+
+    End Sub
+
+    ' Save button click event with Try-Catch for error handling
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Try
+            ' Add save logic here (example: updating dataset back to database)
+            MessageBox.Show("Changes saved successfully.")
+        Catch ex As Exception
+            MessageBox.Show("Error saving changes: " & ex.Message)
+        End Try
+    End Sub
+
+    ' Query selection from ComboBox or buttons
+    Private Sub cmbQuery_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbQuery.SelectedIndexChanged
+        Dim query As String = ""
+
+        Select Case cmbQuery.SelectedIndex
+            Case 0
+                ' Create table for user preferences
+                query = "CREATE TABLE UserGamePreferences (" &
+                "PreferenceID INT PRIMARY KEY IDENTITY(1,1), " &
+                "UserID INT, GameTitle NVARCHAR(100), " &
+                "Platform NVARCHAR(50), Rating NVARCHAR(10), PreferredPrice DECIMAL(5,2));"
+
+            Case 1
+                ' Insert new game into Games table
+                query = "INSERT INTO Games (Title, Platform, Rating, Price, NewUsed, Quantity) " &
+                "VALUES ('Cyberpunk 2077', 'XB', 'M', 49.99, 'N', 10);"
+
+            Case 2
+                ' Delete a game from the catalog
+                query = "DELETE FROM Games WHERE Title = 'Madden NFL 20' AND Platform = 'PS';"
+
+            Case 3
+                ' Update game price
+                query = "UPDATE Games SET Price = 29.99 WHERE Title = 'Resident Evil' AND Platform = 'PS';"
+
+            Case 4
+                ' Sort games by price
+                query = "SELECT Title, Platform, Price FROM Games ORDER BY Price ASC;"
+        End Select
+
+
+
+
+        ' Execute query and update dataset
+        Dim cmd As New SqlCommand(query, conn)
+        Dim adapter As New SqlDataAdapter(cmd)
+        dsGalore.Clear()
+        adapter.Fill(dsGalore, "TableName")
+    End Sub
+
+    ' Close button event to close the application
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Application.Exit()
+    End Sub
+
+
+End Class
+
+
